55ad4b32a6bd4f805cb905e8254d48500af41830
josergdev <joserga@ext.inditex.com>
2023-06-10 17:14:51 +0200

    feat: hello world

---
+++ src/main/java/dev/joserg/communify/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package dev.joserg.communify.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+  @GetMapping("/")
+  public String hello() {
+    return "Hello World!";
+  }
+}
