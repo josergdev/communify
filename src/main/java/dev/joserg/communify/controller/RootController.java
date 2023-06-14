b77e9ffa9a0752505a48c3e7d4cf44905b1fd408
josergdev <joserga@ext.inditex.com>
2023-06-14 10:45:22 +0200

    fix: public resource

--- src/main/java/dev/joserg/communify/controller/RootController.java
+++ src/main/java/dev/joserg/communify/controller/RootController.java
@@ -1,25 +1,25 @@
 package dev.joserg.communify.controller;
 
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 public class RootController {
 
-    @GetMapping("/public")
-    public Map<String, Object> publicResource(JwtAuthenticationToken jwtAuthenticationToken) {
-        return jwtAuthenticationToken.getTokenAttributes();
-    }
+  @GetMapping("/public")
+  public String publicResource() {
+    return "This is a public resource";
+  }
 
-    @GetMapping("/private")
-    @PreAuthorize("hasAuthority('SCOPE_Root.Private')")
-    public Map<String, Object> privateResource(JwtAuthenticationToken jwtAuthenticationToken) {
-        return jwtAuthenticationToken.getTokenAttributes();
-    }
+  @GetMapping("/private")
+  @PreAuthorize("hasAuthority('SCOPE_Root.Private')")
+  public Map<String, Object> privateResource(JwtAuthenticationToken jwtAuthenticationToken) {
+    return jwtAuthenticationToken.getTokenAttributes();
+  }
 }
