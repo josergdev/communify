ffed8c7859a281452734fdd18418e723e50c47dc
josergdev <joserga@ext.inditex.com>
2023-06-14 09:16:25 +0200

    chore: security

--- src/main/java/dev/joserg/communify/configuration/WebSecurityConfiguration.java
+++ src/main/java/dev/joserg/communify/configuration/WebSecurityConfiguration.java
@@ -1,7 +1,5 @@
 package dev.joserg.communify.configuration;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,20 +9,22 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .cors(CorsConfigurer::disable)
-        .csrf(CsrfConfigurer::disable)
-        .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/actuator/**").permitAll()
-            .anyRequest().authenticated())
-        .oauth2ResourceServer(oauth -> oauth.jwt(withDefaults()));
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt(withDefaults()))
+                .build();
+    }
 }
