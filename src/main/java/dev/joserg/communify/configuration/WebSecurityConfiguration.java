5889b20aae9aa6d844817212f46fe7e3073b457f
josergdev <joserodriguezgarcia95@gmail.com>
2023-06-18 00:36:25 +0200

    feat: add version public endpoint

--- src/main/java/dev/joserg/communify/configuration/WebSecurityConfiguration.java
+++ src/main/java/dev/joserg/communify/configuration/WebSecurityConfiguration.java
@@ -1,7 +1,5 @@
 package dev.joserg.communify.configuration;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,21 +9,23 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .cors(CorsConfigurer::disable)
-        .csrf(CsrfConfigurer::disable)
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/actuator/**").permitAll()
-            .requestMatchers("/public/**").permitAll()
-            .anyRequest().authenticated())
-        .oauth2ResourceServer(oauth -> oauth.jwt(withDefaults()))
-        .build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt(withDefaults()))
+                .build();
+    }
 }
