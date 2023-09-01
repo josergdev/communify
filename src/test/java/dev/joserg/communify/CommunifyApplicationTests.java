edecf4e32510381bebda5d975f309f1a9628924c
José Rodríguez <115999458+josergaut@users.noreply.github.com>
2023-09-01 18:43:14 +0200

    feat: update workflows (#5)
    * feat: update workflows
    
    * fix: action
    
    * fix: action
    
    * fix: action

--- src/test/java/dev/joserg/communify/CommunifyApplicationTests.java
+++ src/test/java/dev/joserg/communify/CommunifyApplicationTests.java
@@ -1,13 +1,11 @@
 package dev.joserg.communify;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CommunifyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
