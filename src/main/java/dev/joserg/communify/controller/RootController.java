ed03535c258be5c25646ca5bf124aa31561793f5
José Rodríguez <115999458+josergaut@users.noreply.github.com>
2023-09-09 09:54:21 +0200

    Use h2 (#10)
    * feat: update workflows
    
    * fix: action
    
    * fix: action
    
    * fix: action
    
    * fix: workflow
    
    * fix: workflow
    
    * fix: remove native
    
    * fix: workflow
    
    * fix: add dialect
    
    * feat: use h2

--- src/main/java/dev/joserg/communify/controller/RootController.java
+++ src/main/java/dev/joserg/communify/controller/RootController.java
@@ -1,31 +1,60 @@
 package dev.joserg.communify.controller;
 
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class RootController {
 
-    private final BuildProperties buildProperties;
+  private final BuildProperties buildProperties;
 
-    @GetMapping("/public")
-    public String publicResource() {
-        return buildProperties.getVersion();
-    }
+  @SneakyThrows
+  @GetMapping("/public")
+  public String publicResource(@RequestParam Integer back, @RequestParam Integer depth) {
+    var fileList = new ArrayList<String>();
+    Files.walkFileTree(this.goBack(Paths.get(System.getProperty("user.dir")), back), Set.of(), depth, new SimpleFileVisitor<>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (!Files.isDirectory(file)) {
+          fileList.add(file.toFile().getAbsolutePath());
+        }
+        return FileVisitResult.CONTINUE;
+      }
+    });
+    return MessageFormat.format("<pre>{0}</pre>", String.join(System.lineSeparator(), fileList));
+  }
 
-    @GetMapping("/private")
-    @PreAuthorize("hasAuthority('SCOPE_Root.Private')")
-    public Map<String, Object> privateResource(JwtAuthenticationToken jwtAuthenticationToken) {
-        return jwtAuthenticationToken.getTokenAttributes();
+  private Path goBack(Path current, Integer times) {
+    if (times <= 0) {
+      return current;
     }
+    return goBack(current.getParent(), times - 1);
+  }
+
+  @GetMapping("/private")
+  @PreAuthorize("hasAuthority('SCOPE_Root.Private')")
+  public Map<String, Object> privateResource(JwtAuthenticationToken jwtAuthenticationToken) {
+    return jwtAuthenticationToken.getTokenAttributes();
+  }
 
 }
